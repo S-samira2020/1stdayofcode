b31b43b64a50b954011c22e3c1d3d7b1b4c1e637
Samira <54632872+S-samira2020@users.noreply.github.com>
2021-07-20 01:50:59 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,62 @@
 using namespace std;
 int main()
 {
+   /// Drawing a shape
+    cout << "|     |" << endl;
+    cout << "|     |" << endl;
+    cout << "|_____|" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+
+
+// taking variable
+    string name = "Samira";
+    int age = 21;
+    cout << "There is a girl, her name is: " << name << endl;
+    cout << "She is " << age << " years old and supremely lazy" << endl;
+    cout << "She wanted to do something good but can't do it she is good for nothing" << endl;
+    cout << "She don't like her name : " << name << " at all" << endl;
+
+
+
+/// data type;
+
+    char ch = 'A';
+    string s = "Samira";
+    int i = 10;
+    float f = 10.5;
+    bool is_female  = true;
+    cout << ch << endl;
+    cout << s << endl;
+    cout << i << endl;
+    cout << f << endl;
+    cout << is_female << endl;
+
+
+
+
+///working with strings
+
+    string s = "Happy Coding";
+              //  ||||||||||||\\
+               // 0123456789101112
+    cout << s << endl;
+    // printing length of this string
+    cout << s.length() << endl;
+    cout << s[0] <<endl; // 1st index
+    s[0] = 'Y';
+    cout << s << endl;
+    cout << s.find("Coding", 0) << endl;
+    string subs;
+    subs = s.substr(6,3);
+    cout << subs << endl;
+
+
+
+
+
+
    int a = 10;
    int mul= 10*5;
    a++;
@@ -13,5 +69,6 @@ int main()
    float b = 7.5;
    b++;
    cout << b << endl;
+
     return 0;
 }
