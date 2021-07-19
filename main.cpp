c8910285d7c0963044fa70acf111db6e4762af2a
Samira <54632872+S-samira2020@users.noreply.github.com>
2021-07-20 01:49:13 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a = 10;
+   int mul= 10*5;
+   a++;
+   cout << a <<endl;
+   cout << mul <<endl;
+   float f  = 5.5;
+   float mul1 = 5.5 * 1.2;
+   cout << mul1 << endl;
+   float b = 7.5;
+   b++;
+   cout << b << endl;
+    return 0;
+}
